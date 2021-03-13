491cf561f6c88c63541f597833bf150cc1a27b2d
Namazbayev Almas <55187430+Almas016@users.noreply.github.com>
2021-03-13 21:15:16 +0600

    Update 13.sql

--- SQLqueries/13.sql
+++ SQLqueries/13.sql
@@ -1,3 +1,1 @@
-.......................
-update users table -> is Payhed 
--> is_oayed + tarif_if
+update users set tarif_id = ? WHERE id in (select book_id from book_user where user_id in (select id from users)) 
