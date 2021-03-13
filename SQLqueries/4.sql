fa2c5917edc09e187edaa5a5838cac7b62653e58
180107099 <55269444+180107099@users.noreply.github.com>
2021-03-13 22:57:58 +0600

    Update 4.sql

--- SQLqueries/4.sql
+++ SQLqueries/4.sql
@@ -1,3 +1,4 @@
+--How can a publisher publish or update books by certain authors or authors?
 insert into books select request->title, request->description, request->img, from dual 
 where (select book_id from author_book where author_id = request->author_id);
 
