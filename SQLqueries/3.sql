d5712023fc37da378525e19c211b6b17554a66a6
Namazbayev Almas <55187430+Almas016@users.noreply.github.com>
2021-03-13 19:45:39 +0600

    Create 3.sql

---
+++ SQLqueries/3.sql
@@ -0,0 +1,4 @@
+INSERT INTO authors(name,surname,title,img) VALUES (value1, value2, value3, value4);
+UPDATE authors SET name = new-value1, surname = new-value2, title = new-value3, img = new-value4
+WHERE id = author_id; 
+--author_id = request -> id
