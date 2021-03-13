209d7c6d49a5a285bda01755d66a176bb62c1cfd
180107099 <55269444+180107099@users.noreply.github.com>
2021-03-13 23:00:49 +0600

    Update 10.sql

--- SQLqueries/10.sql
+++ SQLqueries/10.sql
@@ -1,2 +1,3 @@
+--How the user will leave feedback (comments, likes, dislikes) about the book?
 insert into comments select request->text from dual 
 where (select * from comments where book_id = request->id);
