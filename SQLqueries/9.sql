a96097dc003277e05ee7333795bae10cb29ec897
180107099 <55269444+180107099@users.noreply.github.com>
2021-03-13 23:00:18 +0600

    Update 9.sql

--- SQLqueries/9.sql
+++ SQLqueries/9.sql
@@ -1,3 +1,4 @@
+--How the user can search for books by author, by genre, by keywords in the catalog, in their library, and in a separate section?
 select * from books where id in (select book_id from author_book where author_id =request->id)
 select * from books where id in (select book_id from book_section where section_id = request->id)
 SELECT * FROM books  where title like '%request->title%' or description like '%request->description%'
