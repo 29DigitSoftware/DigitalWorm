2699d77f2b667548945280c56ad5cdf74b1eb1ac
180107099 <55269444+180107099@users.noreply.github.com>
2021-03-13 23:02:07 +0600

    Update 13.sql

--- SQLqueries/13.sql
+++ SQLqueries/13.sql
@@ -1,1 +1,2 @@
+--How the user pays for the service and at what rate (for a month, for six months, for a year)?
 update users set tarif_id = ? WHERE id in (select book_id from book_user where user_id in (select id from users)) 
