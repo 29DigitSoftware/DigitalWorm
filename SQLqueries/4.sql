653e50e587b26185ddb0897b6c36b83b5bc77f39
Namazbayev Almas <55187430+Almas016@users.noreply.github.com>
2021-03-13 20:03:27 +0600

    Create 4.sql

---
+++ SQLqueries/4.sql
@@ -0,0 +1,7 @@
+insert into books select request->title, request->description, request->img, from dual 
+where (select book_id from author_book where author_id = request->author_id);
+
+update books set title = new-value1 
+and description = new-value2
+and img = new-value3
+where (select book_id from author_book where author_id = request->author_id);
