256a27d8ba0ab87d3f4cca93b1fac03f8ac780d8
180107099 <55269444+180107099@users.noreply.github.com>
2021-03-13 22:59:21 +0600

    Update 7.sql

--- SQLqueries/7.sql
+++ SQLqueries/7.sql
@@ -1,2 +1,3 @@
+--How can a publisher group books by specific genres, topics, and display them in the catalog?
 SELECT * FROM books where id in (select book_id from book_section where section_id = request->id) 
 and where id in (select book_id from book_user where user_id in (select id from users)) 
