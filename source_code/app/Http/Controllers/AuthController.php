c0498791089787d59b590d3af2ab5684196b5f03
PolyAtomicIon <180107265@stu.sdu.edu.kz>
2021-04-03 10:01:12 +0600

    changes

--- source_code/app/Http/Controllers/AuthController.php
+++ source_code/app/Http/Controllers/AuthController.php
@@ -2,19 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+use App\User;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
-    {
+    public function login(Request $request) {
         $response = [];
-        $user = (object)DB::select("select * from users where email=" . $request->email . " LIMIT 1");
-
-        if (!$user || !Hash::check($request->password, $user->password)) {
+        $user = User::where('email', $request->email)->first();
+    
+        if (! $user || ! Hash::check($request->password, $user->password)) {
             $response['message'] = 'Invalid credentials';
             return response(json_encode($response, JSON_UNESCAPED_UNICODE), 422);
         }
@@ -23,11 +23,10 @@ class AuthController extends Controller
         return response(json_encode($response, JSON_UNESCAPED_UNICODE), 200);
     }
 
-    public function logout(Request $request)
-    {
-        $user = (object)DB::select("select * from users where email=" . $request->email . " LIMIT 1");
-
-        if (!$user || !Hash::check($request->password, $user->password)) {
+    public function logout(Request $request) {
+        $user = User::where('email', $request->email)->first();
+    
+        if (! $user || ! Hash::check($request->password, $user->password)) {
             $response['message'] = 'Invalid credentials';
             return response(json_encode($response, JSON_UNESCAPED_UNICODE), 422);
         }
@@ -35,33 +34,31 @@ class AuthController extends Controller
         return response(json_encode(['message' => 'Success'], JSON_UNESCAPED_UNICODE), 200);
     }
 
-    public function get_token(Request $request)
-    {
+    public function get_token(Request $request) {
         $response = [];
-        $user = (object)DB::select("select * from users where email=" . $request->email . " LIMIT 1");
-
-        if (!$user || !Hash::check($request->password, $user->password)) {
+        $user = User::where('email', $request->email)->first();
+    
+        if (! $user || ! Hash::check($request->password, $user->password)) {
             $response['message'] = 'Invalid credentials';
             return response(json_encode($response, JSON_UNESCAPED_UNICODE), 422);
         }
-
+        
         $response['token'] = $user->tokens()->where('id', 7)->get();
         return response(json_encode($response, JSON_UNESCAPED_UNICODE), 200);
     }
 
-    public function register(Request $request)
-    {
+    public function register(Request $request) {
         $request->validate([
             'email' => 'email:rfc'
         ]);
-        if (((object)DB::select("select * from users where email=" . $request->email . " LIMIT 1")) != null) {
+        if (User::where([['email', '=', $request->email]])->first() != null) {
             $response = ['message' => 'User already exist'];
         } else {
             $user = new User;
-            $user->name = $request->name;
-            $user->email = $request->email;
-            $user->password = Hash::make($request->password);
-            $user->save();
+            $user -> name = $request -> name;
+            $user -> email = $request -> email;
+            $user -> password = Hash::make($request -> password);
+            $user -> save();
             $response = ['token' => $user->createToken('reader')->plainTextToken];
         }
         return response(json_encode($response, JSON_UNESCAPED_UNICODE), 200);
